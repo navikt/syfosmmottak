11fc7a4c3ab5a3b5de1ba9889ade738bf2fce3ab
Joakim kartveit <joakim.kartveit@nav.no>
2018-08-21 12:51:18 +0200

    init commit

---
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -0,0 +1,19 @@
+package no.nav.syfo
+
+data class Environment(
+    val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
+    val srvappnameUsername: String = getEnvVar("SRVAPPNAME_USERNAME"),
+    val srvappnamePassword: String = getEnvVar("SRVAPPNAME_PASSWORD"),
+    val mqQueueManagerName: String = getEnvVar("MQGATEWAY04_NAME"),
+    val mqHostname: String = getEnvVar("MQGATEWAY04_HOSTNAME"),
+    val mqPort: Int = getEnvVar("MQGATEWAY04_PORT").toInt(),
+    val mqGatewayName: String = getEnvVar("MQGATEWAY04_NAME"),
+    val mqChannelName: String = getEnvVar("#APP_NAME#"),
+    val srvappserverUsername: String = getEnvVar("SRVAPPSERVER_USERNAME", "srvappserver"),
+    val srvappserverPassword: String = getEnvVar("SRVAPPSERVER_PASSWORD", ""),
+    val sampleQueueQueuename: String = getEnvVar("SAMPLE_QUEUE_QUEUENAME"),
+    val kafkaBootstrapServers: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS_URL")
+)
+
+fun getEnvVar(varName: String, defaultValue: String? = null) =
+        System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Missing required variable \"$varName\"")
