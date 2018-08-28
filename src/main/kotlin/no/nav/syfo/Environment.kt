1a06ca5fe384f3e094a193aa1d4d07aa823f0a79
Joakim kartveit <joakim.kartveit@nav.no>
2018-08-28 10:06:37 +0200

    ny topic navn

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -14,7 +14,7 @@ data class Environment(
     val syfomottakinputQueueName: String = getEnvVar("SYFOMOTTAK_INPUT_QUEUE_QUEUENAME"),
     val syfoSykemeldingRegelerApiURL: String = getEnvVar("SYFO_SYKEMELDINGREGLER_API_URL", "http://syfosykemeldingregler:8080"),
     val apprecQueue: String = getEnvVar("MOTTAK_QUEUE_UTSENDING_QUEUENAME"),
-    val kafkaSM2013JournalfoeringTopic: String = getEnvVar("KAFKA_SM2013_JOURNALING_TOPIC", "privat-syfomottak-journalFoer"),
+    val kafkaSM2013JournalfoeringTopic: String = getEnvVar("KAFKA_SM2013_JOURNALING_TOPIC", "privat-syfomottak-sm2013-journalfoerJoark"),
     val kafkaSM2013LagOppgaveTopic: String = getEnvVar("KAFKA_SM2013_LAGOPPGAVE_TOPIC", "privat-syfomottak-lageoppgave"),
     val syfomottakinputBackoutQueueName: String = getEnvVar("SYFOMOTTAK_BACKOUT_QUEUE_QUEUENAME"),
     val redisHost: String = getEnvVar("REDIS_HOST")
