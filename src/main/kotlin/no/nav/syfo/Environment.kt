3aa1a06e1b007bb8f19b1c13248d89d7c075fb59
Joakim kartveit <joakim.kartveit@nav.no>
2018-12-07 15:22:08 +0100

    Fikset på lintKotlinMain..

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -14,7 +14,7 @@ data class Environment(
     val syfosmmottakinputQueueName: String = getEnvVar("SYFOSMMOTTAK_INPUT_QUEUE_QUEUENAME", "inputQueue"),
     val syfoSmRegelerApiURL: String = getEnvVar("SYFO_SYKEMELDINGREGLER_API_URL", "syfosmregler"),
     val apprecQueue: String = getEnvVar("MOTTAK_QUEUE_UTSENDING_QUEUENAME", "appreQue"),
-    val redisHost: String = getEnvVar("REDIS_HOST","redisHost"),
+    val redisHost: String = getEnvVar("REDIS_HOST", "redisHost"),
     val aktoerregisterV1Url: String = getEnvVar("AKTOERREGISTER_API_V1", "https://app-q1.adeo.no/aktoerregister/api/v1"), // TODO: Make this more naiserator friendly
     val sm2013ManualHandlingTopic: String = getEnvVar("SM2013_MANUAL_HANDLING_TOPIC", "privat-syfo-sm2013-manuellBehandling"),
     val sm2013AutomaticHandlingTopic: String = getEnvVar("SM2013_AUTOMATIC_HANDLING_TOPIC", "privat-syfo-sm2013-automatiskBehandling")
--- src/test/kotlin/no/nav/syfo/utils/kafaUtilConsumer.kt
+++ src/test/kotlin/no/nav/syfo/utils/kafaUtilConsumer.kt
@@ -6,9 +6,9 @@ import java.util.Properties
 import kotlin.reflect.KClass
 
 fun readConsumerConfig(
-        env: Environment,
-        valueDeserializer: KClass<out Deserializer<out Any>>,
-        keyDeserializer: KClass<out Deserializer<out Any>> = valueDeserializer
+    env: Environment,
+    valueDeserializer: KClass<out Deserializer<out Any>>,
+    keyDeserializer: KClass<out Deserializer<out Any>> = valueDeserializer
 ) = Properties().apply {
     load(Environment::class.java.getResourceAsStream("/kafka_consumer.properties"))
     this["sasl.jaas.config"] = "org.apache.kafka.common.security.plain.PlainLoginModule required " +
