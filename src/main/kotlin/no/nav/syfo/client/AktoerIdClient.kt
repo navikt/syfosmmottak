f4706cae9709a886b6c90ee0ca8e0d322e6f3025
Joakim kartveit <joakim.kartveit@nav.no>
2019-02-11 16:32:28 +0100

    Removed CIO import

--- src/main/kotlin/no/nav/syfo/client/AktoerIdClient.kt
+++ src/main/kotlin/no/nav/syfo/client/AktoerIdClient.kt
@@ -2,8 +2,6 @@ package no.nav.syfo.client
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.engine.config
 import io.ktor.client.features.json.JacksonSerializer
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.logging.DEFAULT
