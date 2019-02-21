f6182421625522a9b6197bdc1aa8c05e0f80d09c
Joakim kartveit <joakim.kartveit@nav.no>
2019-02-21 11:43:41 +0100

    Added notifySyfoService now going to mock app in prod

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -276,10 +276,8 @@ fun CoroutineScope.listen(
                     log.info("Apprec Receipt sent to {} $logKeys", config.apprecQueueName, *logValues)
                     kafkaproducer.send(ProducerRecord(config.sm2013AutomaticHandlingTopic, receivedSykmelding))
                     log.info("Message send to kafka {} $logKeys", config.sm2013AutomaticHandlingTopic, *logValues)
-                    /*
                     notifySyfoService(session, syfoserviceProducer, ediLoggId, healthInformation)
                     log.info("Message send to syfo {} $logKeys", config.syfoserviceQueueName, *logValues)
-                    */
                     val currentRequestLatency = requestLatency.observeDuration()
                     log.info("Message $logKeys has outcome automatic, processing took {}s", *logValues,
                             currentRequestLatency)
@@ -289,9 +287,8 @@ fun CoroutineScope.listen(
                     log.info("Apprec Receipt sent to {} $logKeys", config.apprecQueueName, *logValues)
                     kafkaproducer.send(ProducerRecord(config.sm2013ManualHandlingTopic, receivedSykmelding))
                     log.info("Message send to kafka {} $logKeys", config.sm2013ManualHandlingTopic, *logValues)
-                    /* notifySyfoService(session, syfoserviceProducer, ediLoggId, healthInformation)
+                    notifySyfoService(session, syfoserviceProducer, ediLoggId, healthInformation)
                     log.info("Message send to syfo {} $logKeys", config.syfoserviceQueueName, *logValues)
-                    */
                     val currentRequestLatency = requestLatency.observeDuration()
                     log.info("Message $logKeys has outcome manual processing, processing took {}s", *logValues,
                             currentRequestLatency)
