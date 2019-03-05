26656bb0061299097a3d0085f856d095b4b17849
Joakim kartveit <joakim.kartveit@nav.no>
2019-03-05 10:21:14 +0100

    Bumping SyfoOppgave Schemas to verison 1.2-SNAPSHOT

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -478,7 +478,7 @@ fun createTask(kafkaProducer: KafkaProducer<String, ProduceTask>, receivedSykmel
         opprettetAvEnhetsnr = "9999"
         behandlesAvApplikasjon = "FS22" // Gosys
         orgnr = receivedSykmelding.legekontorOrgNr ?: ""
-        beskrivelse = "Manuell behandling sykmelding: ${results.ruleHits.joinToString(", ", prefix = "\"", postfix = "\"")}\""
+        beskrivelse = "Manuell behandling sykmelding: ${results.ruleHits}"
         temagruppe = "SYM"
         tema = ""
         behandlingstema = "BEH_EL_SYM"
