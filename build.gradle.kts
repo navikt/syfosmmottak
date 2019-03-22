9e936c1f40f93b9eb60e14588e1d8e967de106ed
Joakim kartveit <joakim.kartveit@nav.no>
2019-03-22 10:39:04 +0100

    Bumping version to 1.0.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import no.nils.wsdl2java.Wsdl2JavaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "no.nav.syfo"
-version = "1.0.21"
+version = "1.0.22"
 
 val artemisVersion = "2.6.4"
 val coroutinesVersion = "1.0.1"
