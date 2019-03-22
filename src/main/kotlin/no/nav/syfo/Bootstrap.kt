957ac7e21ea65f3f2d5a9ec545259efeb9d14f5c
Joakim kartveit <joakim.kartveit@nav.no>
2019-03-22 14:57:15 +0100

    Changed massageid to receivedSykmelding.sykmelding.id

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -503,7 +503,7 @@ fun CoroutineScope.fetchBehandlendeEnhetAsync(arbeidsfordelingV1: Arbeidsfordeli
 
 fun createTask(kafkaProducer: KafkaProducer<String, ProduceTask>, receivedSykmelding: ReceivedSykmelding, results: ValidationResult, navKontor: String, logKeys: String, logValues: Array<StructuredArgument>) {
     kafkaProducer.send(ProducerRecord("aapen-syfo-oppgave-produserOppgave", receivedSykmelding.msgId, ProduceTask().apply {
-        messageId = receivedSykmelding.msgId
+        messageId = receivedSykmelding.sykmelding.id
         aktoerId = receivedSykmelding.sykmelding.pasientAktoerId
         tildeltEnhetsnr = navKontor
         opprettetAvEnhetsnr = "9999"
