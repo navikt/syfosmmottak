7b646ad43144e5e194c3cfd091819dd096b35e1a
Joakim kartveit <joakim.kartveit@nav.no>
2019-04-05 16:05:42 +0200

    Fixed test sykemeldingmapper

--- src/test/kotlin/no/nav/syfo/SykmeldingMapperSpek.kt
+++ src/test/kotlin/no/nav/syfo/SykmeldingMapperSpek.kt
@@ -1,23 +1,60 @@
 package no.nav.syfo
 
+import no.nav.helse.sm2013.Address
 import no.nav.helse.sm2013.ArsakType
 import no.nav.helse.sm2013.CS
 import no.nav.helse.sm2013.CV
 import no.nav.helse.sm2013.HelseOpplysningerArbeidsuforhet
 import no.nav.helse.sm2013.Ident
+import no.nav.helse.sm2013.NavnType
+import no.nav.helse.sm2013.TeleCom
 import no.nav.syfo.model.toSykmelding
 import org.amshove.kluent.shouldEqual
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import java.time.LocalDate
+import java.time.LocalDateTime
 
-object SykmeldingsMapperSpek : Spek({
+object SykmeldingMapperSpek : Spek({
 
     describe("Check sykmeldings mapping") {
 
         it("Validate MedisinskeArsaker Arsakskode is mapped") {
 
+            val medisinskeArsakerArsakskodeV = "1"
+
             val healthInformation = HelseOpplysningerArbeidsuforhet().apply {
+                arbeidsgiver = HelseOpplysningerArbeidsuforhet.Arbeidsgiver().apply {
+                    harArbeidsgiver = CS().apply {
+                        dn = "En arbeidsgiver"
+                        v = "1"
+                    }
+                    navnArbeidsgiver = "SAS as"
+                    yrkesbetegnelse = "Pilot"
+                    stillingsprosent = 100
+                }
+                kontaktMedPasient = HelseOpplysningerArbeidsuforhet.KontaktMedPasient().apply {
+                    kontaktDato = LocalDate.now()
+                    behandletDato = LocalDateTime.now()
+                }
+                behandler = HelseOpplysningerArbeidsuforhet.Behandler().apply {
+                    navn = NavnType().apply {
+                        fornavn = "Per"
+                        etternavn = "Hansne"
+                    }
+                    id.add(Ident().apply {
+                        id = "12343567"
+                        typeId = CV().apply {
+                            dn = "Fødselsnummer"
+                            s = "2.16.578.1.12.4.1.1.8116"
+                            v = "FNR"
+                        }
+                    })
+                    adresse = Address().apply {
+                    }
+                    kontaktInfo.add(TeleCom().apply {
+                    })
+                }
                 aktivitet = HelseOpplysningerArbeidsuforhet.Aktivitet().apply {
                     periode.add(
                             HelseOpplysningerArbeidsuforhet.Aktivitet.Periode().apply {
@@ -26,7 +63,7 @@ object SykmeldingsMapperSpek : Spek({
                                 aktivitetIkkeMulig = HelseOpplysningerArbeidsuforhet.Aktivitet.Periode.AktivitetIkkeMulig().apply {
                                     medisinskeArsaker = ArsakType().apply {
                                         arsakskode.add(CS().apply {
-                                            v = "1"
+                                            v = medisinskeArsakerArsakskodeV
                                             dn = "Helsetilstanden hindrer pasienten i å være i aktivitet"
                                         })
                                         beskriv = "Kan ikkje jobbe"
@@ -55,6 +92,10 @@ object SykmeldingsMapperSpek : Spek({
                         }
                     }
                 }
+                avsenderSystem = HelseOpplysningerArbeidsuforhet.AvsenderSystem().apply {
+                    systemNavn = "EPJ helse"
+                    systemVersjon = "1.0.2"
+                }
             }
 
             val sykmelding = healthInformation.toSykmelding(
@@ -64,7 +105,7 @@ object SykmeldingsMapperSpek : Spek({
                     msgId = "12313-12313-123123as-asda"
             )
 
-            sykmelding.perioder.first().aktivitetIkkeMulig?.medisinskArsak?.arsak?.first()?.codeValue shouldEqual "1"
+            sykmelding.perioder.first().aktivitetIkkeMulig?.medisinskArsak?.arsak?.first()?.codeValue shouldEqual medisinskeArsakerArsakskodeV
         }
     }
 })
