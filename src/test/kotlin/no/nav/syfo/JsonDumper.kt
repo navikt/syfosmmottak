f4485b45640fe912a0abfb25210889b516f940b4
Joakim kartveit <joakim.kartveit@nav.no>
2019-04-29 08:35:48 +0200

    Added a complete ReceivedSykmelding json dump

--- src/test/kotlin/no/nav/syfo/JsonDumper.kt
+++ src/test/kotlin/no/nav/syfo/JsonDumper.kt
@@ -5,17 +5,42 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import no.nav.helse.sm2013.HelseOpplysningerArbeidsuforhet
+import no.nav.syfo.model.ReceivedSykmelding
 import no.nav.syfo.model.toSykmelding
+import no.nav.syfo.utils.getFileAsString
+import no.trygdeetaten.xml.eiff._1.XMLEIFellesformat
 import java.io.StringReader
 import java.time.LocalDateTime
 
 fun main() {
 
-    val sm = fellesformatUnmarshaller.unmarshal(StringReader(BootstrapSpek::class.java.getResourceAsStream("/generated_sm.xml").readAllBytes().toString(Charsets.UTF_8))) as HelseOpplysningerArbeidsuforhet
-    println(ObjectMapper()
+    val objectMapper: ObjectMapper = ObjectMapper()
+            .registerModule(JavaTimeModule())
             .registerKotlinModule()
-            .registerModule(JavaTimeModule()).apply {
-                configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-            }
-            .writeValueAsString(sm.toSykmelding("detteerensykmeldingid", "41234123", "12890371", "123124334", LocalDateTime.now())))
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+
+    val sm = fellesformatUnmarshaller.unmarshal(StringReader(BootstrapSpek::class.java.getResourceAsStream("/generated_sm.xml").readAllBytes().toString(Charsets.UTF_8))) as HelseOpplysningerArbeidsuforhet
+    println(objectMapper.writeValueAsString(sm.toSykmelding("detteerensykmeldingid", "41234123", "12890371", "123124334", LocalDateTime.now())))
+
+    val stringInput = getFileAsString("src/test/resources/sykemelding2013Regelsettversjon2.xml")
+    val inputMessageText = fellesformatUnmarshaller.unmarshal(StringReader(stringInput)) as XMLEIFellesformat
+
+    val receivedSykmelding = ReceivedSykmelding(
+            sykmelding = sm.toSykmelding("detteerensykmeldingid", "41234123", "12890371", "123124334", LocalDateTime.now()),
+            personNrPasient = "1231231",
+            personNrLege = "123134",
+            navLogId = "4d3fad98-6c40-47ec-99b6-6ca7c98aa5ad",
+            msgId = "06b2b55f-c2c5-4ee0-8e0a-6e252ec2a550",
+            legekontorOrgNr = "444333",
+            legekontorOrgName = "Helese sentar",
+            legekontorHerId = "33",
+            legekontorReshId = "1313",
+            mottattDato = LocalDateTime.now(),
+            rulesetVersion = "2",
+            fellesformat = objectMapper
+                    .writeValueAsString(inputMessageText)
+
+    )
+
+    println(objectMapper.writeValueAsString(receivedSykmelding))
 }
