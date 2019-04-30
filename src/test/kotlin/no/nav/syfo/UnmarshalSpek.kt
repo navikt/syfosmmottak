e1f150152d723910aa3673804c2a597a5f86995b
Joakim kartveit <joakim.kartveit@nav.no>
2019-04-30 12:12:15 +0200

    Added more test for unmarshaller dates

--- src/test/kotlin/no/nav/syfo/UnmarshalSpek.kt
+++ src/test/kotlin/no/nav/syfo/UnmarshalSpek.kt
@@ -18,10 +18,46 @@ object UnmarshalSpek : Spek({
             expectedTomDate shouldEqual healthInformation.aktivitet.periode.first().periodeTOMDato
         }
 
-        it("Test unmarshal dates testsett 1") {
+        it("Test unmarshal dates testsett 2") {
             val healthInformation = fellesformatUnmarshaller.unmarshal(StringReader(BootstrapSpek::class.java.getResourceAsStream("/helseopplysninger-ISO-8859-1.xml").readAllBytes().toString(Charsets.ISO_8859_1))) as HelseOpplysningerArbeidsuforhet
             val expectedFomDate = LocalDate.of(2017, 9, 1)
             val expectedTomDate = LocalDate.of(2017, 10, 27)
+
+            expectedFomDate shouldEqual healthInformation.aktivitet.periode.first().periodeFOMDato
+            expectedTomDate shouldEqual healthInformation.aktivitet.periode.first().periodeTOMDato
+        }
+
+        it("Test unmarshal dates testsett 3") {
+            val healthInformation = fellesformatUnmarshaller.unmarshal(StringReader(BootstrapSpek::class.java.getResourceAsStream("/generated_sm_2.xml").readAllBytes().toString(Charsets.ISO_8859_1))) as HelseOpplysningerArbeidsuforhet
+            val expectedFomDate = LocalDate.of(-44, 1, 1)
+            val expectedTomDate = LocalDate.of(-44, 1, 5)
+
+            expectedFomDate shouldEqual healthInformation.aktivitet.periode.first().periodeFOMDato
+            expectedTomDate shouldEqual healthInformation.aktivitet.periode.first().periodeTOMDato
+        }
+
+        it("Test unmarshal dates testsett 4") {
+            val healthInformation = fellesformatUnmarshaller.unmarshal(StringReader(BootstrapSpek::class.java.getResourceAsStream("/generated_sm_3.xml").readAllBytes().toString(Charsets.ISO_8859_1))) as HelseOpplysningerArbeidsuforhet
+            val expectedFomDate = LocalDate.of(12004, 4, 12)
+            val expectedTomDate = LocalDate.of(12004, 4, 20)
+
+            expectedFomDate shouldEqual healthInformation.aktivitet.periode.first().periodeFOMDato
+            expectedTomDate shouldEqual healthInformation.aktivitet.periode.first().periodeTOMDato
+        }
+
+        it("Test unmarshal dates testsett 5") {
+            val healthInformation = fellesformatUnmarshaller.unmarshal(StringReader(BootstrapSpek::class.java.getResourceAsStream("/generated_sm_4.xml").readAllBytes().toString(Charsets.ISO_8859_1))) as HelseOpplysningerArbeidsuforhet
+            val expectedFomDate = LocalDate.of(2004, 4, 12)
+            val expectedTomDate = LocalDate.of(2004, 4, 20)
+
+            expectedFomDate shouldEqual healthInformation.aktivitet.periode.first().periodeFOMDato
+            expectedTomDate shouldEqual healthInformation.aktivitet.periode.first().periodeTOMDato
+        }
+
+        it("Test unmarshal dates testsett 6") {
+            val healthInformation = fellesformatUnmarshaller.unmarshal(StringReader(BootstrapSpek::class.java.getResourceAsStream("/generated_sm_4.xml").readAllBytes().toString(Charsets.ISO_8859_1))) as HelseOpplysningerArbeidsuforhet
+            val expectedFomDate = LocalDate.of(2004, 4, 12)
+            val expectedTomDate = LocalDate.of(2004, 4, 20)
 
             expectedFomDate shouldEqual healthInformation.aktivitet.periode.first().periodeFOMDato
             expectedTomDate shouldEqual healthInformation.aktivitet.periode.first().periodeTOMDato
