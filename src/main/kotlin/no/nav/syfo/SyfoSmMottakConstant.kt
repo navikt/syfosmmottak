34e099e2a68bb1f233df6b4b86e18294e47f3269
Kevin Sillerud <TheUnnamedDude@users.noreply.github.com>
2019-07-03 15:19:17 +0200

    Fix messagetype for apprecs, should be all capital

--- src/main/kotlin/no/nav/syfo/SyfoSmMottakConstant.kt
+++ src/main/kotlin/no/nav/syfo/SyfoSmMottakConstant.kt
@@ -1,7 +1,7 @@
 package no.nav.syfo
 
 enum class SyfoSmMottakConstant(val string: String) {
-    apprec("apprec"),
+    apprec("APPREC"),
     apprecVersionV1_0("1.0 2004-11-21"),
     ebRoleNav("Saksbehandler"),
     ebServiceSykmelding("Sykmelding"),
