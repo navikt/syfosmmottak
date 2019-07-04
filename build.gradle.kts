a5b77ada48cbd388f99006552c13c8924f4118c9
Kevin Sillerud <kevin.sillerud@gmail.com>
2019-07-04 17:39:14 +0200

    Bump kotlinter version

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ plugins {
     java
     id("no.nils.wsdl2java") version "0.10"
     kotlin("jvm") version "1.3.40"
-    id("org.jmailen.kotlinter") version "1.21.0"
+    id("org.jmailen.kotlinter") version "1.26.0"
     id("com.diffplug.gradle.spotless") version "3.23.1"
     id("com.github.johnrengelman.shadow") version "4.0.4"
 }
