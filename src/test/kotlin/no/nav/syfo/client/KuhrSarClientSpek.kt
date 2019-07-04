5fb8021794787f11ff91ddbe99604bf31e89e676
Kevin Sillerud <kevin.sillerud@gmail.com>
2019-07-04 17:13:45 +0200

    Clean up best samhandler praksis and add tests

---
+++ src/test/kotlin/no/nav/syfo/client/KuhrSarClientSpek.kt
@@ -0,0 +1,51 @@
+package no.nav.syfo.client
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.amshove.kluent.shouldBeLessThan
+import org.amshove.kluent.shouldEqual
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import kotlin.test.fail
+
+object KuhrSarClientSpek : Spek({
+
+    val objectMapper = ObjectMapper()
+            .registerKotlinModule()
+            .registerModule(JavaTimeModule())
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+    describe("KuhrSarClient") {
+        val samhandler: List<Samhandler> = objectMapper.readValue(KuhrSarClientSpek::class.java.getResourceAsStream("/kuhr_sahr_response.json").readBytes().toString(Charsets.UTF_8))
+
+        it("Finner en aktiv samhandler praksis") {
+            val match = findBestSamhandlerPraksis(samhandler, "SomeInvalidName", null)
+                    ?: fail("Unable to find samhandler praksis")
+            match.percentageMatch shouldBeLessThan 50.0
+        }
+
+        it("Foretrekker samhandler praksisen med en matchende her id selv om navnet er likt") {
+            val match = findBestSamhandlerPraksis(samhandler, "Testlegesenteret", "12345")
+                    ?: fail("Unable to find samhandler praksis")
+            match.percentageMatch shouldEqual 100.0
+            match.samhandlerPraksis.samh_praksis_id shouldEqual "1000456788"
+        }
+
+        it("Finner en samhandler praksis når navnet matcher 100%") {
+            val match = findBestSamhandlerPraksis(samhandler, "Testlegesenteret", null)
+                    ?: fail("Unable to find samhandler praksis")
+            match.percentageMatch shouldEqual 100.0
+            match.samhandlerPraksis.samh_praksis_id shouldEqual "1000456789"
+        }
+
+        it("Finner en samhandler praksis når her iden ikke matcher") {
+            val match = findBestSamhandlerPraksis(samhandler, "Testlegesenteret", "23456")
+                    ?: fail("Unable to find samhandler praksis")
+            match.percentageMatch shouldEqual 100.0
+            match.samhandlerPraksis.samh_praksis_id shouldEqual "1000456789"
+        }
+    }
+})
