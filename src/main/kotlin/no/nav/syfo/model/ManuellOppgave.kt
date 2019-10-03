9ea014c12a5b3d7ac91bacc8fe6b106911edbb36
Joakim Kartveit <joakimkartveit@gmail.com>
2019-10-03 16:53:41 +0200

    Legger til støtte for å sende enkelte manuelle oppgaver til syfosmmanuell

---
+++ src/main/kotlin/no/nav/syfo/model/ManuellOppgave.kt
@@ -0,0 +1,10 @@
+package no.nav.syfo.model
+
+import no.nav.syfo.apprec.Apprec
+
+data class ManuellOppgave(
+    val receivedSykmelding: ReceivedSykmelding,
+    val validationResult: ValidationResult,
+    val apprec: Apprec,
+    val behandlendeEnhet: String
+)
