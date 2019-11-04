155ccd885b66f925e5cb5cb2d5e1f69e846c9955
Joakim Kartveit <joakimkartveit@gmail.com>
2019-11-04 08:49:57 +0100

    Tar og legger til av vi avvist test fnr som er kommet in i produksjon

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -60,6 +60,7 @@ import no.nav.syfo.handlestatus.handlePatientNotFoundInAktorRegister
 import no.nav.syfo.handlestatus.handleStatusINVALID
 import no.nav.syfo.handlestatus.handleStatusMANUALPROCESSING
 import no.nav.syfo.handlestatus.handleStatusOK
+import no.nav.syfo.handlestatus.handleTestFnrInProd
 import no.nav.syfo.kafka.loadBaseConfig
 import no.nav.syfo.kafka.toProducerConfig
 import no.nav.syfo.metrics.AVVIST_ULIK_SENDER_OG_BEHANDLER
@@ -408,6 +409,12 @@ suspend fun blockingApplicationLogic(
                         continue@loop
                     }
 
+                    if (erTestFnr(personNumberPatient) && env.cluster == "prod-fss") {
+                        handleTestFnrInProd(loggingMeta, fellesformat,
+                                ediLoggId, msgId, msgHead, env, kafkaproducerApprec, jedis, sha256String)
+                        continue@loop
+                    }
+
                     val sykmelding = healthInformation.toSykmelding(
                             sykmeldingId = UUID.randomUUID().toString(),
                             pasientAktoerId = patientIdents.identer!!.first().ident,
@@ -559,4 +566,15 @@ fun aktivitetIkkeMuligMissingArbeidsplassenArsakskode(aktivitetIkkeMulig: HelseO
     else if (aktivitetIkkeMulig.arbeidsplassen != null && aktivitetIkkeMulig.arbeidsplassen.arsakskode == null)
         true
     else aktivitetIkkeMulig.arbeidsplassen != null && aktivitetIkkeMulig.arbeidsplassen.arsakskode.any { it.v.isNullOrEmpty() }
+}
+
+fun erTestFnr(fnr: String): Boolean {
+    val testFnr = listOf("14077700162", "23077200290", "19095800273", "19079800468", "09090950972", "16126800464",
+    "19128600143", "22047800106", "21016400952", "07070750710", "12119000465", "15040650560", "28027000608",
+    "13031353453", "15045400112", "04129700489", "12050050295", "02117800213", "18118500284", "03117000205",
+    "08077000292", "20086600138", "02039000183", "11028600374", "13046700125", "28096900254", "14076800236",
+    "03117800252", "11079500412", "15076500565", "15051555535", "21030550231", "13116900216", "04056600324",
+    "14019800513", "05073500186", "12057900499", "24048600332", "17108300566", "01017112364", "11064700342",
+    "29019900248", "25047039315")
+    return testFnr.contains(fnr)
 }
