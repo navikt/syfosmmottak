d930d173a5f5f8e0d7a7a4a65791cdea75168d29
andreasnilsen <danduras@gmail.com>
2021-03-26 13:38:49 +0100

    Fix timestamp parsing and log date

--- src/main/kotlin/no/nav/syfo/util/XMLDateTimeAdapter.kt
+++ src/main/kotlin/no/nav/syfo/util/XMLDateTimeAdapter.kt
@@ -5,22 +5,23 @@ import com.migesok.jaxb.adapter.javatime.LocalDateXmlAdapter
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneOffset
+import java.util.GregorianCalendar
 import javax.xml.bind.DatatypeConverter
 import no.nav.syfo.log
 
 class XMLDateTimeAdapter : LocalDateTimeXmlAdapter() {
     override fun unmarshal(stringValue: String?): LocalDateTime? = when (stringValue) {
         null -> null
-        else -> {
-            log.info("received timestamp $stringValue")
-            DatatypeConverter.parseDateTime(stringValue).toInstant().atZone(ZoneOffset.UTC).toLocalDateTime()
-        }
+        else -> (DatatypeConverter.parseDateTime(stringValue) as GregorianCalendar).toZonedDateTime().toLocalDateTime()
     }
 }
 
 class XMLDateAdapter : LocalDateXmlAdapter() {
     override fun unmarshal(stringValue: String?): LocalDate? = when (stringValue) {
         null -> null
-        else -> DatatypeConverter.parseDate(stringValue).toInstant().atZone(ZoneOffset.MAX).toLocalDate()
+        else -> {
+            log.info("Received date $stringValue")
+            DatatypeConverter.parseDate(stringValue).toInstant().atZone(ZoneOffset.MAX).toLocalDate()
+        }
     }
 }
