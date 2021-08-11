532eddaae8ba706be7d70cbd9c18871a1fe7dc03
Mathias Hellevang <mathias.hellevang@gmail.com>
2021-08-11 13:11:43 +0200

    Revert "Revert "Merge pull request #160 from navikt/azureadpdl""
    This reverts commit 398bd7d88d01c842ab90d74e3e73db31ffbc1e34.

--- src/main/kotlin/no/nav/syfo/pdl/service/PdlPersonService.kt
+++ src/main/kotlin/no/nav/syfo/pdl/service/PdlPersonService.kt
@@ -2,16 +2,20 @@ package no.nav.syfo.pdl.service
 
 import io.ktor.util.KtorExperimentalAPI
 import net.logstash.logback.argument.StructuredArguments
-import no.nav.syfo.client.StsOidcClient
+import no.nav.syfo.client.AccessTokenClientV2
 import no.nav.syfo.log
 import no.nav.syfo.pdl.client.PdlClient
 import no.nav.syfo.util.LoggingMeta
 
 @KtorExperimentalAPI
-class PdlPersonService(private val pdlClient: PdlClient, private val stsOidcClient: StsOidcClient) {
+class PdlPersonService(
+    private val pdlClient: PdlClient,
+    private val accessTokenClientV2: AccessTokenClientV2,
+    private val pdlScope: String
+) {
 
     suspend fun getAktorids(identer: List<String>, loggingMeta: LoggingMeta): Map<String, String?> {
-        val stsToken = stsOidcClient.oidcToken().access_token
+        val stsToken = accessTokenClientV2.getAccessTokenV2(pdlScope)
         val pdlResponse = pdlClient.getAktorids(identer, stsToken)
 
         if (pdlResponse.errors != null) {
