28856fa7b488a420664f4ed2454dcd91bfe7cf4c
Andreas Sagen Aspaas <danduras@gmail.com>
2024-02-26 15:49:29 +0100

    chore: remove syfosm-common

--- src/main/kotlin/no/nav/syfo/bootstrap/KafkaClients.kt
+++ src/main/kotlin/no/nav/syfo/bootstrap/KafkaClients.kt
@@ -1,15 +1,32 @@
 package no.nav.syfo.bootstrap
 
+import java.util.Properties
+import kotlin.reflect.KClass
 import no.nav.syfo.EnvironmentVariables
 import no.nav.syfo.apprec.Apprec
 import no.nav.syfo.kafka.aiven.KafkaUtils
-import no.nav.syfo.kafka.toProducerConfig
 import no.nav.syfo.model.ManuellOppgave
 import no.nav.syfo.model.OpprettOppgaveKafkaMessage
 import no.nav.syfo.model.ReceivedSykmelding
 import no.nav.syfo.model.ValidationResult
 import no.nav.syfo.util.JacksonKafkaSerializer
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.Serializer
+import org.apache.kafka.common.serialization.StringSerializer
+
+fun Properties.toProducerConfig(
+    groupId: String,
+    valueSerializer: KClass<out Serializer<out Any>>,
+    keySerializer: KClass<out Serializer<out Any>> = StringSerializer::class
+): Properties =
+    Properties().also {
+        it.putAll(this)
+        it[ConsumerConfig.GROUP_ID_CONFIG] = groupId
+        it[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = valueSerializer.java
+        it[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = keySerializer.java
+    }
 
 class KafkaClients(environmentVariables: EnvironmentVariables) {
 
